1c8e792819a19e5bb94c933446fc6f7c34ca436e
v-aguiar <v.aguiar.dev@gmail.com>
2022-03-16 12:51:38 -0300

    Add homepage component. Also add  initial styles files, icons and fonts

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+﻿import HomePage from "./HomePage";
+
+export default function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+﻿export default function HomePage() {
+  function startRecall() {
+    document.querySelector("#homepage").classList.add("--hidden");
+  }
+
+  return (
+    <article id="homepage">
+      <img src="assets/logo.svg" alt="ZapRecall Logo" />
+      <h1 className="logo-text --first-screen">ZapRecall</h1>
+
+      <button className="homepage-button" onClick={() => startRecall()}>Iniciar Recall!</button>
+    </article>
+  )
+}
