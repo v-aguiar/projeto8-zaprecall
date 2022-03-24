880d3dbf3cd58c40ee155261469be09d1c432ab0
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2022-03-24 09:54:07 -0300

    Preparando revisão de código

--- src/components/CardButtons.jsx
+++
@@ -1,9 +0,0 @@
-﻿export default function CardButtons({turnCardBack}) {
-  return (
-    <div className="card-buttons">
-      <button onClick={() => turnCardBack("red")} className="btn --red">Não lembrei</button>
-      <button onClick={() => turnCardBack("orange")} className="btn --orange">Quase não lembrei</button>
-      <button onClick={() => turnCardBack("green")} className="btn --green">Zap ⚡</button>
-    </div>
-  )
-}
